c39f46bda8baef43ebda0db660a048ae76a48fb4
phutiwanich@gmail.com <phutiwanich@gmail.com>
2025-06-16 17:24:59 +0700

    id!

--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { features, testimonials } from '../Assets/Assets';
 
-const Services = () => {
+const Services = ({id}) => {
     return (
-        <div className="max-w-6xl mx-auto p-4 select-none">
+        <div id={id} className="max-w-6xl mx-auto p-4 select-none">
             {/* title section */}
             <div className="text-center py-20 px-4">
                 <div className="mb-4">
