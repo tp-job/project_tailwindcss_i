697d3dbcb0d4ebde60497e594fc75cfc1cb69cc9
phutiwanich@gmail.com <phutiwanich@gmail.com>
2025-06-16 18:01:38 +0700

    href!

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const Footer = ({id}) => {
+const Footer = () => {
     return (
-        <div id={id} className="mt-20">
+        <div className="mt-20">
             <div className="text-center">
                 <div className="flex items-center gap-2 mx-auto mt-4 text-xl w-max">
                     <box-icon name='envelope' type='solid' ></box-icon>
@@ -12,18 +12,18 @@ const Footer = ({id}) => {
             <div className="text-center sm:flex items-center justify-between border-t border-r-gray-400 mx-[10%] mt-12 py-6">
                 <p>2025 Nevinas. All rights reserved.</p>
                 <ul className="flex items-center justify-center gap-10 mt-4 sm:mt-0">
-                    <li className="">
-                        <a href="https://x.com/JobTh_Z" className="">
+                    <li>
+                        <a href="#" className="">
                             <i className="fa-brands fa-x-twitter"></i>
                         </a>
                     </li>
-                    <li className="">
-                        <a href="https://www.instagram.com/tp_job_th/?hl=en" className="">
+                    <li>
+                        <a href="#" className="">
                             <i className="fa-brands fa-instagram"></i>
                         </a>
                     </li>
-                    <li className="">
-                        <a href="https://github.com/tp-job" className="">
+                    <li>
+                        <a href="#" className="">
                             <i className="fa-brands fa-github"></i>
                         </a>
                     </li>
